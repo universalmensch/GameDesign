992269dd6b8593f6b3e2a2c7ce2725965489808c
kk <krausko@outlook.de>
2026-03-21 18:37:18 +0100

    feat: tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rigidbody;
     private float movementX;
     private float movementY;
+    public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,6 +35,25 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate() {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rigidbody.AddForce(movement);
+        rigidbody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count;
+        if (count >= 10)
+        {
+            winText.SetActive(true);
+        }
     }
 }
