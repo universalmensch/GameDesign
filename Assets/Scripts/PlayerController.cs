1d17d5d28b29dcfec2d26419613ee26683d66389
kk <krausko@outlook.de>
2026-03-31 15:17:16 +0200

    feat: changed player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rigidbody;
     private float movementX;
     private float movementY;
+    private float movementZ;
     public float speed = 0;
     private int count;
     public TextMeshProUGUI countText;
     public GameObject winText;
+    private static readonly Vector3 ResetVector = new Vector3(0.0f, 15.0f, 0.0f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,21 +23,34 @@ public class PlayerController : MonoBehaviour
         winText.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rigidbody.transform.GetPositionAndRotation(out var currentPosition, out _);
+        if (currentPosition.y < -10.0f)
+        {
+            rigidbody.transform.SetPositionAndRotation(ResetVector, Quaternion.identity);
+            rigidbody.linearVelocity = Vector3.zero;
+        }
     }
 
-    void OnMove(InputValue movementValue) {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVectorInput = movementValue.Get<Vector2>();
+        movementX = movementVectorInput.x;
+        movementY = movementVectorInput.y;
+
+        rigidbody.transform.GetPositionAndRotation(out var currentPosition, out _);
+        
+        if (currentPosition.y < 30.0f)
+            movementZ = 1.5f;
     }
 
     void FixedUpdate() {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(movementX, movementZ, movementY);
         rigidbody.AddForce(movement * speed);
+        movementZ = 0.0f;
+        movementX = 0.0f;
+        movementY = 0.0f;
     }
 
     private void OnTriggerEnter(Collider other)
